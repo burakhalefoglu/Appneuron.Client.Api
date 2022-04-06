4c2f803766346e5d7e3134582f39ed306b6bec5a
burakhalefoglu <burakhalefoglu@gmail.com>
2022-04-06 20:48:42 +0300

    Update AdvStrategyBehaviorModel.cs and k8s.yaml

--- Entities/Concrete/AdvStrategyBehaviorModel.cs
+++ Entities/Concrete/AdvStrategyBehaviorModel.cs
@@ -5,10 +5,10 @@ namespace Entities.Concrete;
 public class AdvStrategyBehaviorModel : IEntity
 {
     public long Id { get; set; }
+    public long ProjectId { get; set; }
+    public long ClientId { get; set; }
     public long StrategyId { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
-    public long ClientId { get; set; }
-    public long ProjectId { get; set; }
     public int Version { get; set; }
     public string Name { get; set; }
     public bool Status { get; set; }
